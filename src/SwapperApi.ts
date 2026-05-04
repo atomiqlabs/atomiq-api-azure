3efc53a2ec01b78d32e369224c9294c65baf9efc
adambor <adamborteam@gmail.com>
2026-05-04 16:05:48 +0200

    Use v1 solana initializer for mainnet version

--- src/SwapperApi.ts
+++ src/SwapperApi.ts
@@ -1,7 +1,7 @@
 import {BitcoinNetwork, SwapperFactory} from "@atomiqlabs/sdk";
 import {StarknetInitializer} from "@atomiqlabs/chain-starknet";
 import {SwapperApi} from "@atomiqlabs/sdk/api";
-import {SolanaInitializerV2} from "@atomiqlabs/chain-solana";
+import {SolanaInitializer} from "@atomiqlabs/chain-solana";
 import {AlpenInitializer, BotanixInitializer, CitreaInitializer, GoatInitializer} from "@atomiqlabs/chain-evm";
 import {CosmosDBStorageManager, CosmosDBSwapPatchStorage} from "@atomiqlabs/storage-cosmosdb";
 
@@ -30,7 +30,7 @@ const bitcoinNetwork = BITCOIN_NETWORK==null || BITCOIN_NETWORK === "MAINNET"
 
 const chains = [
     StarknetInitializer,
-    SolanaInitializerV2,
+    SolanaInitializer,
     BotanixInitializer,
     CitreaInitializer,
     AlpenInitializer,
